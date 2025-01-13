52d44883670c47ab628b643de3a0b25713f6d889
SajjalMalik <sajjalmalik.sm@gmail.com>
2025-01-13 21:21:27 +0500

    Modified project name

--- 11.Todo App using Django DRF/myenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Sajal\Documents\Django Projects\Todo App using Django DRF\myenv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
